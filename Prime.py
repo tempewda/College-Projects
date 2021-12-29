81156bcfa84d5c2b2b208166a5ed43c08e3d587f
Tanmoy <tanmoyroy1811@gmail.com>
2021-12-29 23:45:44 +0530

    added the last 5 questions

--- Prime.py
+++ Prime.py
@@ -1,10 +1,14 @@
 # program to check if a number is prime or not
 
 number = int(input("Enter a number: "))
-factor = 0
+factor = 0 # store the number of factors
+
+# count the number of factors
 for i in range(1, number + 1):
     if number % i == 0:
         factor += 1
+
+# check the number of factors
 if factor == 2:
     print(f"{number} is a Prime Number")
 else:
